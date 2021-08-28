8eadccd140f320eb95be1d27ffee76a3c263d670
aarvinrathod <aarvinrathod@gmail.com>
2021-08-27 22:29:38 -0500

    08/27/2021

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,32 +1,33 @@
-// Creating the map object
-var myMap = L.map("map", {
-    center: [42.97, -56.69],
-    zoom: 2
+// Adding the tile layer
+let streetmap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+let topomap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
-// Adding the tile layer
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(myMap);
+let baseMaps = {"Street View" : streetmap,
+                "Topographical View" : topomap}
 
+let colors = ["#DCDCDC", "#A9A9A9", "#696969", "#778899", "#191970", "#000000"]              
 
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
-function markerSize(data) {return data * 60000};
+function markerSize(data) {return data * 70000};
 
 function color_choice(data) {
 
-    if (data > -10 & data <= 10) return "#7FFF00";
-    else if (data > 10 & data <= 30) return "#008000";
-    else if (data > 30 & data <= 50) return "#FFD700";
-    else if (data > 50 & data <= 70) return "#FF8C00";
-    else if (data > 70 & data <= 90) return "#CD5C5C";
-    else return "#8B0000";
+    if (data > -10 & data <= 10) return "#DCDCDC";
+    else if (data > 10 & data <= 30) return "#A9A9A9";
+    else if (data > 30 & data <= 50) return "#696969";
+    else if (data > 50 & data <= 70) return "#778899";
+    else if (data > 70 & data <= 90) return "#6495ED";
+    else return "#A52A2A";
     };
 
 
 d3.json(url).then(data => {
-    console.log(data.features[50])
     
     let earthquake_markers = [];
     let points = data.features;
@@ -39,18 +40,62 @@ d3.json(url).then(data => {
                             
                             earthquake_markers.push(
                                                 L.circle(coords, {
-                                                    stroke: false,
-                                                    fillOpacity: 0.75,
-                                                    color: "black",
+                                                    stroke: true,
+                                                    color:"black",
+                                                    weight:0.25,
+                                                    fillOpacity: 1,
                                                     fillColor: color_choice(point.geometry.coordinates[2]),
-                                                    radius: markerSize(point.properties.mag)
+                                                    radius: markerSize(point.properties.mag),
+                                                    interactive: true
                                                     }));
                                 });
 
     let earthquake_marker_group = L.layerGroup(earthquake_markers)
     let overlayMaps = {"earthquakes" : earthquake_marker_group}
-    L.control.layers(overlayMaps).addTo(myMap);
-});
+    // Creating the map object
+    var myMap = L.map("map", {
+    center: [42.97, -56.69],
+    zoom: 2,
+    layers: [topomap, earthquake_marker_group]
+    });
+    L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+
+
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+      var limits = ["-10 to 10", "10 to 30", "30 to 50", "50 to 70", "70 to 90", "90+"];
+      var colors = ["#DCDCDC", "#A9A9A9", "#696969", "#778899", "#6495ED", "#A52A2A"];
+      var labels = [];
+      
+      var legendInfo = "<h3>Earth Depth</h3>"
+
+      div.innerHTML = legendInfo;
+
+      limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\">"+ limits[index] +"</li>");
+      });
+  
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+      return div;
+    };
+  
+    // Adding the legend to the map
+    legend.addTo(myMap);
+    
+    
+  });
+
+// let Legend = L.addLegend("bottomright", 
+// colors =["#DCDCDC", "#A9A9A9", "#696969", "#778899", "#191970", "#000000"],
+// labels= ["-10 to 10", "10 to 30", "30 to 50", "50 to 70", "70 to 90", "90+"],
+// title= "Depth of Earth",
+// opacity = 1)
+
+// Legend.addTo(myMap)
 
 
 // polyline //
+
